d9d69821f5e3bedc339278ac866f7f8388717c2c
Ferrari.20190 <Ferrari.20190@LABI2-13PC11>
2023-01-13 12:19:07 +0100

    Aggiungere i file di progetto.

---
+++ esercizio21_ferrari/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercizio21_ferrari
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[10];
+        }
+    }
+}
